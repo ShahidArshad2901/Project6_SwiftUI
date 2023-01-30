e22773d82f631b234b0063b8b8296dc02bd415d4
Shahid Arshad <shahid.arshad@devsinc.com>
2023-01-30 18:24:09 +0500

    Binding animation with Stepper

--- Animation/ContentView.swift
+++ Animation/ContentView.swift
@@ -10,18 +10,22 @@ import SwiftUI
 struct ContentView: View {
     @State private var animationAmount = 1.0
     var body: some View {
-        Button("Tap Me") {
-            //d0 Noth...
-            animationAmount += 1
+        VStack {
+            Stepper("Scale amount", value: $animationAmount.animation(
+                .easeInOut(duration: 5)
+                .repeatCount(3, autoreverses: true)), in: 1...10)
+            
+            Spacer()
+            
+            Button("Tap Me") {
+                animationAmount += 1
+            }
+            .padding(50)
+            .background(.red)
+            .foregroundColor(.white)
+            .clipShape(Circle())
+            .scaleEffect(animationAmount)
         }
-        .padding(50)
-        .background(.red)
-        .foregroundColor(.white)
-        .clipShape(Circle())
-        .scaleEffect(animationAmount)
-        .blur(radius: (animationAmount-1)*3)
-        .animation(.easeInOut(duration: 0.4).repeatForever(autoreverses: true), value: animationAmount)
-        
     }
 }
 
